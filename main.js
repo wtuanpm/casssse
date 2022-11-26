9e59d6018f09f4c640ae0d2dfa4e4ae26967ee7d
Tuan pham <phamtuan@Phams-MacBook-Pro.local>
2022-11-26 18:04:56 +0700

    calculate monthly payment

---
+++ main.js
@@ -0,0 +1,40 @@
+var interestElement = document.getElementById("interest");
+var monthlyPaymentElement = document.getElementById('monthly-payment');
+
+var sixMonths = 6;
+var twelveMonths = 12;
+var twentyFourMonths = 24;
+
+
+function calculatePaidMonthly() {
+    var interestRate1 = 10;
+    var interestRate2 = 12;
+    var interestRate3 = 14;
+
+    var amountElement = document.getElementById('amount');
+    var tenorElement = document.getElementById('tenor');
+    var amount= Number(amountElement.value); 
+    var tenor = Number(tenorElement.value);
+
+    var interestRate = 10;
+    if (tenor == sixMonths) {
+        interestElement.value = interestRate1 + "%";
+        interestRate = interestRate1;
+    } else if (tenor == sixMonths) {
+        interestElement.value = interestRate2 + "%";
+        interestRate = interestRate1;
+
+    } else {
+        interestElement.value = interestRate3 + "%";
+        interestRate = interestRate1;
+    }
+
+    var monthlyPaymentAmount = (amount / tenor) * (interestRate / 100)  +   (amount / tenor);
+    monthlyPaymentElement.value = monthlyPaymentAmount;
+    
+}
+
+
+function advide() {
+    alert("Nhân viên của chúng tôi sẽ sớm liên hệ với bạn")
+}
